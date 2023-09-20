5e1d83281a3a011994888bc29d22889a21625cdc
Lara Slullitel <lslullitel@gmail.com>
2023-09-20 13:08:21 -0300

    Proyecto final React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar/NavBar"; 
+
+function App() {
+        return (
+          <div>
+            {/* NavBar */} 
+            <NavBar />
+      
+            {/* ItemListContainer */} 
+            <ItemListContainer greeting="Buenos días! Como no tengo una tienda real para usar para este proyecto, voy a realizar una tienda para un RPG!"/>
+          </div>
+        );
+      }
+
+export default App;
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,31 @@
+import CartWidget from "../CartWidget/CartWidget"; 
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid"></div>
+
+        {/* Brand */}    
+        <a href="#" className="navbar-brand">Lemosian Market</a>
+        
+        {/* Links */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">Weapons</a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">Beasts</a>
+          </li>
+          <li> 
+             <a href="#" className="nav-link">General goods</a>
+           </li>
+        </ul>
+
+        { /* CartWidget*/ }
+        <CartWidget />
+
+        </nav>
+    );
+};
+
+export default NavBar;
