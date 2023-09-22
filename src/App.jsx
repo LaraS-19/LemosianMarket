3e2189a5d99abcd918173e21c442855babb40cee
Lara Slullitel <lslullitel@gmail.com>
2023-09-22 20:02:13 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import NavBar from "./components/NavBar/NavBar"; 
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { Routes, Route } from 'react-router-dom';
+import ItemDetailContainer from "./components/ItemListContainer/ItemDetailContainer";
 
 function App() {
         return (
           <div>
-            {/* NavBar */} 
+           {/* NavBar */} 
             <NavBar />
+
+            <h1>Lemosian Market</h1>
       
             {/* ItemListContainer */} 
             <ItemListContainer greeting="Buenos días! Como no tengo una tienda real para usar para este proyecto, voy a realizar una tienda para un RPG!"/>
+          
+          <Routes>
+            <Route path="/" element = {<ItemListContainer />}></Route>
+            <Route path="/category/:id" element={<ItemListContainer />} ></Route>
+            <Route path ="/item/:id" element={<ItemDetailContainer />} ></Route>
+          </Routes>
           </div>
         );
       }
